17cfef251a9997b8498e86b3252fded5153e68dc
CHESERGIU <sergiu.chereches@gmail.com>
2019-05-14 08:36:58 +0300

    console read data and output result

---
+++ ValidJSON/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ValidJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string console = Console.ReadLine();
+            String errorMessages = null;
+            char[] jsonString = console.ToCharArray();
+            Console.WriteLine(IsValidJSONString(jsonString, errorMessages));
+            Console.ReadLine();
+        }
+
+        private static bool IsValidJSONString(char[] jsonString, string errorMessages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
